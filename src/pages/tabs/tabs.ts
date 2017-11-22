5c96616cd9aaca338980ffc1c5b5ef9a93be537f
Danilo Gomes <danilogomesdev@gmail.com>
2017-11-22 21:08:09 +0000

    refactoring

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FavouritesPage } from '../favourites/favourites';
 import { LibraryPage } from '../library/library';
 
-//
+
 @Component({
   selector : 'tabs-page',
   templateUrl : 'tabs.html'
