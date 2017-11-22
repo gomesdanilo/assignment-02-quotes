debafda0f0008ea63bbf6b1ad71e1f290a702627
Danilo Gomes <danilogomesdev@gmail.com>
2017-11-22 20:51:28 +0000

    working with tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FavouritesPage } from '../favourites/favourites';
 import { LibraryPage } from '../library/library';
 
-// 
+//
 @Component({
   //templateUrl: 'tabs.html'
   selector : 'tabs-page',
