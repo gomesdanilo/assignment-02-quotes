f6a0b1bead296bdc14139dca675f360094b6321d
Danilo Gomes <danilogomesdev@gmail.com>
2017-11-28 21:18:37 +0000

    quotes structure working

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @Component({
@@ -9,7 +9,11 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
   quoteGroup : { category : string, quotes : Quote[], icon : string};
 
-  constructor(private navParams : NavParams){}
+  constructor(
+    private navParams : NavParams,
+    private alertCtrl : AlertController){
+
+  }
 
   ngOnInit(){
     this.quoteGroup = this.navParams.data;
@@ -21,5 +25,29 @@ export class QuotesPage implements OnInit {
   //  // Add elvis operator (?) in template to use this approate.
   // }
 
+  onAddToFavourite(selectorQuote : Quote){
+    const alert = this.alertCtrl.create({
+      title : "Add Quote",
+      subTitle : "Are you sure?",
+      message : "Are you sure you want to add the quote?",
+      buttons : [
+        {
+          text : "Yes, go ahead",
+          handler : () => {
+            console.log("Ok")
+          }
+        },
+        {
+          text : "No, I changed my mind",
+          role : "cancel",
+          handler : () => {
+            console.log("Cancel")
+          }
+        }
+      ]
+
+    });
+    alert.present();
+  }
 
 }
